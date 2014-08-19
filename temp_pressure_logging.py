c8d54d1dff9ae78d0ee4e90374c119eea3971b05
romain <romain@localhost.localdomain>
2014-08-20 00:24:10 +0200

    added argumetn parssing plus default values

--- temp_pressure_logging.py
+++ temp_pressure_logging.py
@@ -1,36 +1,43 @@
 import time
 import os
 from BMP085 import Bmp085
+import argparse
+
+# Parsing command line arguments.
+parser = argparse.ArgumentParser()
+parser.add_argument("--interval", type=int, help="interval between measurements in seconds.", default=30)
+parser.add_argument("--period", type=int, help="Time in seconds for the program to continue measurement.", default=600)
+args = parser.parse_args()
 
 # I2C address of the BMP085 sensor.
 SENSOR_ADDRESS = 0x77
-print "Initialize sensor on I2C address: %s" % SENSOR_ADDRESS
-bmp085 = Bmp085(SENSOR_ADDRESS, 0)
-start_time = time.time()
 temps = []
 pressures = []
-default_interval = 30
-default_average_time = 600
-while time.time() - startdate < default_average_time:
+start_time = time.time()
+
+print "Initialize sensor on I2C address: %s" % SENSOR_ADDRESS
+bmp085 = Bmp085(SENSOR_ADDRESS, 0)
+
+while time.time() - startdate < args.period:
     # read both temp and pressure from sensor.
     (temp, p) = bmp085.readTempPressure()
     # add read values to buffer lists.
     temps.append(temp)
     pressures.append(p)
-    time.sleep(default_interval)
-temp_avg = sum(temps)/len(temps)
-pressure_avg = sum(pressures)/len(pressures)
-
+    # Wait for the set time between intervals.
+    time.sleep(args.interval)
 
 key = time.time()
 now = datetime.now()
 values = [key, now.year, now.month, now.day, now.hour, now.minute, now.second]
-values.append(temp_avg)
-values.append(pressure_avg)
+values.append(sum(temps)/len(temps))
+values.append(sum(pressures)/len(pressures))
 # Convert list to list of str.
 values = ["%s" % x for x in values]
+# Connect to database.
 dbconnection = sqlite3.connect('/home/pi/db/temppressure.db')
 cursor = dbconnection.cursor()
+# Insert data into database.
 cursor.execute("INSERT INTO data VALUES (?,?,?,?,?,?,?,?,?)", values)
 dbconnection.commit()
 cursor.close()
