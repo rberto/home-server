d4f53564afd2f16b34a0e6479c7d637f84a2908c
romain <romain@localhost.localdomain>
2015-04-19 20:15:06 +0200

    update shutclient.
    Signed-off-by: romain <romain@localhost.localdomain>

--- shutclient.py
+++ shutclient.py
@@ -1,6 +1,15 @@
+###
+# Add this scrit to crontab of root of any machine you want to be able to
+# shutdown from get request.
+# And add this line before cron line as cron PATH is restricted.
+# Need to add shutdwon to PATH:
+# PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+###
+
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
+import subprocess
 
 USER = "romain"
 PASSWORD = "aqw"
@@ -14,17 +23,17 @@ class MainHandler(tornado.web.RequestHandler):
         action = str(self.get_argument("action", None))
         if user == USER and password == PASSWORD:
             if action == "shutdown":
-                from subprocess import call
-                call(["shutdown", "-h", "now"])
+                subprocess.Popen(["shutdown -h now"], shell = True)
             else:
                 raise tornado.web.HTTPError(403, "Wrong action!")
         else:
             raise tornado.web.HTTPError(403, "Wrong password and/or username.")
 
 class WebInterface():
+
     def start(self):
         local_app = tornado.web.Application(
-            handlers = [(r"/", MainHandler)])
+            handlers = [(r"/", MainHandler)], debug = False)
 
         local_server = tornado.httpserver.HTTPServer(local_app)
 
